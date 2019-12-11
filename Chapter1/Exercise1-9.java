c117880ff96d7e3c8f06b919eb64c6d2f307e93d
Henopp96 <58237336+Henopp96@users.noreply.github.com>
2019-12-10 18:02:23 -0700

    Update Exercise1-9.java

--- Chapter1/Exercise1-9.java
+++ Chapter1/Exercise1-9.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Cody Henopp
+ 12/9/19
+ 
+ This is a program solving for the area and perimeter of a rectangle
  */
 package javaapplication3;
 
