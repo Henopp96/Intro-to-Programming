cb1179912a2df12188986118e32e7185eb68ead7
Henopp96 <58237336+Henopp96@users.noreply.github.com>
2019-12-10 18:03:19 -0700

    Update Exercise1-5.java

--- Chapter1/Exercise1-5.java
+++ Chapter1/Exercise1-5.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Cody Henopp
+ 12/9/19
+ 
+ This is a program that solves the equation from assignment.
  */
 package javaapplication2;
 
