069dd2eb7bb3f30e8abd8fdd41aa91561f4d7800
Henopp96 <58237336+Henopp96@users.noreply.github.com>
2019-12-10 18:00:50 -0700

    Add files via upload

---
+++ Chapter1/Exercise1-5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author student
+ */
+
+public class JavaApplication2 {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        {System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));}
+    }
+    
+}
