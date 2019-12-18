dd12fbfded4cc784fa66de794b9ef5b2be75637c
Henopp96 <58237336+Henopp96@users.noreply.github.com>
2019-12-18 15:14:07 -0700

    Update Triangle.java

--- Chapter3/Triangle.java
+++ Chapter3/Triangle.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
  */
 public class Triangle {
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Enter first side: ");
 		double side1 = input.nextDouble();
 		System.out.print("Enter second side: ");
@@ -18,32 +18,24 @@ public class Triangle {
 		System.out.print("Enter third side: ");
 		double side3 = input.nextDouble();
 		double perimeter = (side1+side2+side3);
-		
-		
-		if ((side1+side2) <= side3) {
+
+
+
+
+		if ((side1+side3) <= side2 || (side2+side3) <= side1 || (side1+side2) <= side3) {
 
 			System.out.println("Answer is invalid.");
-				
-				if ((side1+side3) <= side2) {
-					
-					System.out.println("Answer is invalid.");
-				}
-				
-				if ((side2+side3) <= side1) {
-					
-					System.out.println("Answer is invalid.");
-				}
-			
 		}
-		
+
+
 		else {
-			
+
 
 			System.out.print(perimeter);
 
-			
+
 		}
-		
+
 	}
 
 }
